9cbf6519389d6aa56796b1239526aa6d93c55523
amna-kamran <jurazilgreen@gmail.com>
2024-04-11 20:33:10 +0500

    image moderation categories

--- app/services/text.py
+++ app/services/text.py
@@ -17,9 +17,25 @@ async def analyze_text():
     async with httpx.AsyncClient() as client:
         try:
             response = await client.post('https://api.sightengine.com/1.0/text/check.json', data=data)
-            response.raise_for_status()  # Raise an exception for HTTP errors
+            response.raise_for_status() 
             output = response.json()
-            return output
+            
+            # check if sexual
+            sexual_prob = output.get("moderation_classes", {}).get("sexual", 0)
+            if sexual_prob > 0:
+                moderate = True
+                return {"moderate": moderate, "moderation_class": "sexual", "probability": sexual_prob}
+
+            # check other categories
+            categories = ["discriminatory", "insulting", "violent", "toxic"]
+            for category in categories:
+                category_prob = output.get("moderation_classes", {}).get(category, 0)
+                if category_prob >= 0.02:
+                    moderate = True
+                    return {"moderate": moderate, "moderation_class": category, "probability": category_prob}
+            
+            return {"moderate": False}
+            
         except httpx.HTTPError as e:
             # Handle HTTP errors
             raise HTTPException(status_code=e.response.status_code, detail=str(e))
